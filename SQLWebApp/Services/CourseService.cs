8fff9e2dd2fc04eb31f19d6b86d9f55c1c161bac
naveen <naveen.kumar@randstad.com.my>
2022-03-06 15:02:09 +0530

    Add project files.

---
+++ SQLWebApp/Services/CourseService.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using SQLWebApp.Models;
+
+namespace SQLWebApp.Services
+{
+    public class CourseService
+    {
+        // Ensure to change the below variables to reflect the connection details for your database
+        private static string db_source = "naveenaz204.database.windows.net";
+        private static string db_user = "naveenaz204";
+        private static string db_password = "Naveen@204";
+        private static string db_database = "naveenaz204";
+
+        private SqlConnection GetConnection(string connectionString)
+        {
+            // Here we are creating the SQL connection
+            //var _builder = new SqlConnectionStringBuilder();
+            //_builder.DataSource = db_source;
+            //_builder.UserID = db_user;
+            //_builder.Password = db_password;
+            //_builder.InitialCatalog = db_database;
+            return new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Course> GetCourses(string connectionString)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT CourseId,CourseName,Rating from Course";
+            SqlConnection _connection = GetConnection(connectionString);
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseId = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+            _connection.Close();
+            return _lst;
+        }
+
+    }
+}
